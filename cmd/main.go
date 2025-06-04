339a746d49d4167def513993690fb4ed1394a32a
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-06-05 07:31:53 +0800

    update: brought back logging

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -13,17 +14,17 @@ import (
 func main() {
 	// Load environment variables
 	loadEnv()
-	
+
 	// Set up logging
-	// setupLogging()
-	
+	setupLogging()
+
 	// Create data directory if it doesn't exist
 	ensureDataDirectory()
-	
+
 	// Run the application
-	// log.Println("Starting Mr Verse application")
+	log.Println("Starting Mr Verse application")
 	ui.RunApp()
-	
+
 	log.Println("Application shutdown complete")
 }
 
@@ -40,31 +41,31 @@ func loadEnv() {
 }
 
 // setupLogging configures the application logging
-// func setupLogging() {
-// // Create logs directory if it doesn't exist
-// logsDir := "logs"
-// if err := os.MkdirAll(logsDir, 0755); err != nil {
-// 		log.Printf("Warning: Could not create logs directory: %v", err)
-// 		return
-// 	}
+func setupLogging() {
+	// Create logs directory if it doesn't exist
+	logsDir := "logs"
+	if err := os.MkdirAll(logsDir, 0755); err != nil {
+		log.Printf("Warning: Could not create logs directory: %v", err)
+		return
+	}
 
-// 	// Set up log file
-// 	logFile := filepath.Join(logsDir, "mr-verse.log")
-// 	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-// 	if err != nil {
-// 		log.Printf("Warning: Could not open log file: %v", err)
-// 		return
-// 	}
+	// Set up log file
+	logFile := filepath.Join(logsDir, "mr-verse.log")
+	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Printf("Warning: Could not open log file: %v", err)
+		return
+	}
 
-// 	// Create a multi-writer to log to both file and stdout
-// 	multiWriter := io.MultiWriter(os.Stdout, f)
-	
-// 	// // Configure the logger to use the multi-writer
-// 	log.SetOutput(multiWriter)
-// 	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
-	
-// 	log.Println("Logging configured to write to both console and", logFile)
-// }
+	// Create a multi-writer to log to both file and stdout
+	multiWriter := io.MultiWriter(os.Stdout, f)
+
+	// // Configure the logger to use the multi-writer
+	log.SetOutput(multiWriter)
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+
+	log.Println("Logging configured to write to both console and", logFile)
+}
 
 // ensureDataDirectory creates the data directory if it doesn't exist
 func ensureDataDirectory() {
@@ -72,7 +73,7 @@ func ensureDataDirectory() {
 	if err := os.MkdirAll(dataDir, 0755); err != nil {
 		log.Fatalf("Error: Could not create data directory: %v", err)
 	}
-	
+
 	// Check if there are any Bible translation files
 	files, err := filepath.Glob(filepath.Join(dataDir, "*.json"))
 	if err != nil {
